606ed443ccf420db6b775fa6359472bca63a772c
Jacopo Mondini <jacopo.mondini@marconi.loc>
2026-09-21 16:12:19 +0200

    backup

--- FirstPrject/src/main.cpp
+++ FirstPrject/src/main.cpp
@@ -4,6 +4,10 @@ void setup() {
   // put your setup code here, to run once:
   //inizializza il monitor seriale
   Serial.begin(115200);
+  // Inizializza il LED integrato e lo imposta sempre acceso
+  int LED_BUILTIN = 2;
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
   /*int count = 10;
   for (int i = 0; i < count; i++)
   {
@@ -18,10 +22,13 @@ void setup() {
     delay(1000);
    }
   */
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("HelloWorld");
   delay(1000);
+  // led integrato lasciato sempre acceso in setup
+
 }
